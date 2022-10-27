5ec1035270c1af2684de73a4f84ba9b9853e0f11
HabeebatulQalb <gahneeyahadeniyi@gmail.com>
2022-10-26 23:01:44 -0700

    models created

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Artiste(models,Model):
+    first_name = models.Charfield(max_length=400)
+    first_name = models.Charfield(max_length=400)
+    
+
+class Song(models,Model):
+    pass
+class lyric(models,Model):
